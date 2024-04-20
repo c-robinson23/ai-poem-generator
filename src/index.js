2f9e19d2c6ad9dfd503e558295a6149ea281efd7
c-robinson23 <147357577+c-robinson23@users.noreply.github.com>
2024-04-19 22:53:24 -0400

    updated UX and design styling

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "8955aec3fa84eee934db0tdod70f6d28";
   let context =
-    "You are a African American poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML an separate each line with a <br />. Never show the word html or ` or ''. Make sure to follow the user instructions.  Sign the poem with <em> element '-SheCode AI' at the end of the peom never in the beginning. Never include a title to the poem.";
+    "You are a African American poem expert and love to write short poems. Your mission is to generate a 6 line poem in basic HTML an separate each line with a <br />. Never show the word html or ` or ''. Make sure to follow the user instructions.  Sign the poem with <em> element '-SheCodes AI' at the end of the peom and Never in the beginning. Never include a title to the poem.";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
